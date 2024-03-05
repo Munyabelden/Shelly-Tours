d48de946e641479b4d962e1fdf2f988b0ce636cc
munyamugb <munyaradzi045@gmail.com>
2024-03-05 11:00:02 +0200

    finish header style

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,10 +18,13 @@ const Home = () => {
       <section className="main m-4 p-4">
         <div className="home-about">
             <span>Get to know us better</span>
-            <h2>Shelly tours live life internationally</h2>
+            <h2>Shelly tours: live life internationally</h2>
             <div className="flex flex-row items-start justify-around">
-              <p>Shelly Tours operates in the thriving airport transfer and tourism industry in Cape Town, South Africa. As an upcoming unlaunched business, we are poised to make a significant impact by providing efficient and reliable services to luxury vacationers in the region. Our distribution channels include both online platforms and a physical location, ensuring convenience and accessibility for our customers. The tourism industry in Cape Town is a key driver of economic growth, attracting millions of visitors each year. With its stunning landscapes, vibrant culture, and rich historey, Cape Town has become a popular destination for both domestic and international travellers. As a result, the demand for airport transfer services has been steadily increasing.</p>
-              <img  src={Shelly} alt="just an image"/>
+              <div>
+                <p>Shelly Tours operates in the thriving airport transfer and tourism industry in Cape Town, South Africa. As an upcoming unlaunched business, we are poised to make a significant impact by providing efficient and reliable services to luxury vacationers in the region. Our distribution channels include both online platforms and a physical location, ensuring convenience and accessibility for our customers. The tourism industry in Cape Town is a key driver of economic growth, attracting millions of visitors each year. With its stunning landscapes, vibrant culture, and rich historey, Cape Town has become a popular destination for both domestic and international travellers. As a result, the demand for airport transfer services has been steadily increasing.</p>
+                <button className="more-about">Find out more {'>'}</button>
+              </div>
+              <img src={Shelly} />
             </div>
         </div>
       </section>
