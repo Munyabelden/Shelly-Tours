c8731de5a0c336ce61a2916afff74e9896cda1a2
munyamugb <munyaradzi045@gmail.com>
2024-03-18 08:14:41 +0200

    Add map

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,14 @@ import './styles/Footer.css';
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3311.9201333269866!2d18.493656299999998!3d-33.891710700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc5c237072a95b%3A0xde9653e2b4786d07!2s290%20Koeberg%20Rd%2C%20Tijgerhof%2C%20Cape%20Town%2C%207441!5e0!3m2!1sen!2sza!4v1710740800185!5m2!1sen!2sza"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
       <div className="footer-details"> 
         <div>
           <h2>Cape Town</h2>
           <p>Discover the breathtaking beauty of Cape Town: where vibrant culture meets stunning landscapes, creating unforgettable memories at the tip of Africa!</p>
+          <p>Email shellytours@gmail.co.za</p>
           <button type="button">Contact Us</button>
         </div>
         <ul>
